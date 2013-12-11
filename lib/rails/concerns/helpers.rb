694fb6be1426fdb74b9e2d3bb787deb37aabf288
Jerod Santo <jerod.santo@gmail.com>
2013-12-11 15:34:04 -0600

    don't require columns when there are migrations pending

--- lib/rails/concerns/helpers.rb
+++ lib/rails/concerns/helpers.rb
@@ -2,6 +2,8 @@ module Rails
   module Concerns
     module Helpers
       def require_column base, column_name
+        return if ActiveRecord::Migrator.needs_migration?
+
         if base.table_exists? and base.columns.map(&:name).exclude?(column_name)
           raise NotImplementedError, "#{base} must have '#{column_name}' column"
         end
