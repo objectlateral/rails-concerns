9b548c1cc338ff1f99a6a3efc639fdd2e25a95f4
Jerod Santo <jerod@objectlateral.com>
2018-06-05 14:14:38 -0500

    Handle newer migration pending API

--- lib/rails/concerns/helpers.rb
+++ lib/rails/concerns/helpers.rb
@@ -2,10 +2,20 @@ module Rails
   module Concerns
     module Helpers
       def require_column base, column_name
-        return if ActiveRecord::Migrator.needs_migration?
+        return if migrations_pending? base
 
         if base.table_exists? and base.columns.map(&:name).exclude?(column_name)
           raise NotImplementedError, "#{base} must have '#{column_name}' column"
+        end
+      end
+
+      private
+
+      def migrations_pending? base
+        if ActiveRecord::Migrator.respond_to? :needs_migration?
+          ActiveRecord::Migrator.needs_migration?
+        else
+          base.connection.migration_context.needs_migration?
         end
       end
     end
